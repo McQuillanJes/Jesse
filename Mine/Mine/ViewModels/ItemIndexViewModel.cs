cd11e79067c8510eb27b9229b5ec5c95da4c15df
McQuillanJes <94885264+McQuillanJes@users.noreply.github.com>
2022-01-24 20:12:45 -0800

    dded Read Method to ItemIndexViewModel

--- Mine/Mine/ViewModels/ItemIndexViewModel.cs
+++ Mine/Mine/ViewModels/ItemIndexViewModel.cs
@@ -54,5 +54,18 @@ namespace Mine.ViewModels
                 IsBusy = false;
             }
         }
+
+        /// <summary>
+        /// Read an item from the datastore
+        /// </summary>
+        /// <param name="id">The ID of the record</param>
+        /// <returns>Returns a record from ReadAsync</returns>
+        public async Task<ItemModel> ReadAsync(string id)
+        {
+            var result = await DataStore.ReadAsync(id);
+
+            return result;
+        }
+
     }
 }
