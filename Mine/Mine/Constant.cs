4053252af86767010ad7ce419622abd0e17f3eba
McQuillanJes <94885264+McQuillanJes@users.noreply.github.com>
2022-01-29 18:51:45 -0800

    Added Constants class to the Shared Project, Constants.cs

---
+++ Mine/Mine/Constant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+
+namespace Game
+{
+    public static class Constants
+    {
+        public const string DatabaseFilename = "game.db3";
+
+        public const SQLite.SQLiteOpenFlags Flags =
+            //Open the databse in read-write mode
+            SQLite.SQLiteOpenFlags.ReadWrite |
+            //Create the databse if it doesn't exist
+            SQLite.SQLiteOpenFlags.Create |
+            //Enable multi-threaded database access
+            SQLite.SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath
+        {
+            get
+            {
+                var basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                return Path.Combine(basePath, DatabaseFilename);
+            }
+        }
+
+    }
+}
