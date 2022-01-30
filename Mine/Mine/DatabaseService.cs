5559e88b9ffc9a7529f8c1b334446f0301aac327
McQuillanJes <94885264+McQuillanJes@users.noreply.github.com>
2022-01-29 20:34:56 -0800

    Added IDataStore to DatabaseService with temporary interfaces in DatabaseService.cs

--- Mine/Mine/DatabaseService.cs
+++ Mine/Mine/DatabaseService.cs
@@ -2,15 +2,14 @@
 using System.Linq;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-using System.Text;
 using SQLite;
 using Mine.Models;
 
-
 namespace Mine.Services
 {
-    public class DatabaseService
+    public class DatabaseService : IDataStore<ItemModel>
     {
+
         static readonly Lazy<SQLiteAsyncConnection> lasyInitializer = new Lazy<SQLiteAsyncConnection>(() =>
         {
             return new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
@@ -36,6 +35,32 @@ namespace Mine.Services
                 initialized = true;
             }
         }
+
+        public Task<bool> CreateAsync(ItemModel item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(ItemModel item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ItemModel> ReadAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ItemModel>> IndexAsync(bool forceRefresh = false)
+        {
+            throw new NotImplementedException();
+        }
+
 
     }
 
