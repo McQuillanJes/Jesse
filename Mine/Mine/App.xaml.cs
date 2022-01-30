2b2c3607e5cd67d3c07a94016796f3471164c43b
McQuillanJes <94885264+McQuillanJes@users.noreply.github.com>
2022-01-29 20:59:53 -0800

    Changed DependencyService to use DatabaseService in App.xaml.cs

--- Mine/Mine/App.xaml.cs
+++ Mine/Mine/App.xaml.cs
@@ -9,11 +9,18 @@ namespace Mine
     public partial class App : Application
     {
 
+        /// <summary>
+        /// I guess this starts the app then
+        /// </summary>
         public App()
         {
             InitializeComponent();
 
-            DependencyService.Register<MockDataStore>();
+            //register the mock data store instead of the database
+            //DependencyService.Register<MockDataStore>();
+
+            //Register the database instead of the mockdatastore
+            DependencyService.Register<DatabaseService>();
             MainPage = new MainPage();
         }
 
